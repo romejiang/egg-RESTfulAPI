8b9c5c1010ad006a93e4ca4a363f63fb182be96d
StephenAustin <stephen.austin.au@gmail.com>
2020-01-20 02:14:53 +0800

    添加系统进程消息监听模板

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// 系统消息监听代码
+module.exports = app => {
+  // app.beforeStart(async () => {
+  //   // ensure memory cache exists before app ready
+  //   await app.runSchedule('force_refresh');
+  // });
+
+  // const { messenger } = app;
+
+  // messenger.on('refresh', data => {
+
+  //   const ctx = app.createAnonymousContext();
+  //   ctx.runInBackground(async () => {
+  //     const nsp = app.io.of('/');
+
+  //     nsp.clients(async (error, clients) => {
+  //       if (error) throw error;
+
+  //       app.logger.info('start send by %s clients' , clients.length);
+  //       if (clients.length > 0) {
+  //         await nsp.emit("miner", data);
+  //       }
+  //     })
+  //   });
+  // });
+};
