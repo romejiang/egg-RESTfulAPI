0f4514ed816332f4ad556bebd88c8695af11dee8
StephenAustin <stephen.austin.au@gmail.com>
2020-01-20 02:08:30 +0800

    添加定时任务模板

---
+++ app/schedule/task.js
@@ -0,0 +1,14 @@
+module.exports = app => {
+  return {
+    schedule: {
+      cron: (app.config.env === 'prod' || app.config.env === 'st') ? '0 0 * * * *' : '0 0 * * * *',
+      immediate: false,
+      type: 'worker',
+    },
+    async task(ctx) {
+      console.log("************ crontab 定时统计 ..." + new Date());
+
+    },
+  };
+};
+
